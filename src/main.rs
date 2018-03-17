f434ec02818ddb4a3e1376d1da426c43b3baa604
mode777 <mode7@gmx.net>
2018-03-17 18:00:27 +0100

    first demo

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#[macro_use] extern crate glium;
+
+fn main() {
+    use glium::{glutin, Surface};
+
+    let mut events_loop = glium::glutin::EventsLoop::new();
+    let window = glium::glutin::WindowBuilder::new();
+    let context = glium::glutin::ContextBuilder::new();
+    let display = glium::Display::new(window, context, &events_loop).unwrap();
+
+    let mut closed = false;
+    while !closed {
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.finish().unwrap();
+
+        events_loop.poll_events(|ev| {
+            match ev {
+                glutin::Event::WindowEvent { event, .. } => match event {
+                    glutin::WindowEvent::Closed => closed = true,
+                    _ => (),
+                },
+                _ => (),
+            }
+        });
+    }
+}
